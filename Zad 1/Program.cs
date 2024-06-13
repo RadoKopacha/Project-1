dcb78d67f895ae3f78648b52e1a9005de2febd7c
RadoKopacha <petyrnikolov0804@gmail.com>
2024-06-13 20:33:22 +0300

    first commit

---
+++ Zad 1/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Zad_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input how many nunbers you want:");
+            int n = int.Parse(Console.ReadLine());
+
+            int[] array = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+            }
+
+            
+           Array.Reverse(array);
+
+            
+            Console.WriteLine(string.Join(" ", array));
+        }
+    }
+}
+
---
+++ Zad 2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Channels;
+
+namespace Zad_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input 30 numbers");
+            int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray(); 
+            int min = nums.Min();
+            int max = nums.Max();
+            int sum = 0;
+            int zeroCount = 0;
+
+            for (int i = 0; i < 30; i++)
+            {
+                
+                if (nums[i] > 0)
+                {
+                    sum += nums[i];
+                }
+                if (nums[i] == 0)
+                {
+                    zeroCount++;
+                }
+            }
+            Console.WriteLine($"Sum is:{sum}");
+            Console.WriteLine($"the lowest element:{min}");
+            Console.WriteLine($"The bigest element:{max}");
+            Console.WriteLine($"The count of the o numbers is:{zeroCount}");
+        }   
+    }
+}
